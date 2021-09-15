ce644cbc515e99904dc260fd819302e6815365f5
Giovanni Gaffe <gaffejonathan@ymail.com>
2021-09-15 17:46:45 +0800

    remove header view code

--- PlantsParadise/Views/HeaderView.swift
+++ PlantsParadise/Views/HeaderView.swift
@@ -9,51 +9,19 @@ import SwiftUI
 
 struct HeaderView: View {
     
-    let addplant = AddPlants()
-    @State private var isPresentedBool = false
-    
     var body: some View {
-        NavigationView {
-            VStack {
-                List {
-                    ForEach(addplant.items, id:\.self) { item in
-                        Text(item.name ?? "no data")
-                    }
-                    .onDelete(perform: removeItem)
-                }.cornerRadius(8)
-                VStack{
-                    Button("Ajouter une plante") {
-                        isPresentedBool.toggle()
-                    }
-                    .padding()
-                    .background(
-                        LinearGradient(gradient: Gradient(colors: [Color(Constant.Color.magentaUIColor), Color(Constant.Color.blueUIColor)]), startPoint: .top, endPoint: .bottom)
-                    )
-                    .cornerRadius(8)
-                    .foregroundColor(.white)
-                }
-                .navigationBarItems(leading:
-                                        HStack {
-                    Image("logo")
-                        .resizable()
-                        .frame(width: 60, height: 60)
-                        .font(.largeTitle)
-                        .navigationBarTitleDisplayMode(.automatic)
-                   
-                    Text("   Plants Paradise")
-                        .font(Font.custom("Didot", size: 30))
-                })
-            }.padding()
-        }
-        .sheet(isPresented: $isPresentedBool) {
-            AddPlants()
-        }
-    }
-    
-    func removeItem(at offsets: IndexSet) {
-        for index in offsets {
-            let item = addplant.items[index]
-            PersistenceController.shared.delete(item)
+        VStack {
+            HStack(alignment: .center) {
+                Image("logo")
+                    .resizable()
+                    .frame(width: 60, height: 60)
+                    .font(.largeTitle)
+                    .navigationBarTitleDisplayMode(.automatic)
+               
+                Text("  Plants Paradise")
+                    .font(Font.custom("Didot", size: 30))
+            }
+            Spacer()
         }
     }
 }
