034309e4ac15bf06c9a7023bd143eba0c721ef46
Giovanni Gaffe <gaffejonathan@ymail.com>
2021-09-15 17:30:18 +0800

    copy code from header view

--- PlantsParadise/Views/WelcomeView.swift
+++ PlantsParadise/Views/WelcomeView.swift
@@ -8,13 +8,62 @@
 import SwiftUI
 
 struct WelcomeView: View {
+    
+    let addplant = AddPlants()
+    @State private var isPresentedBool = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            VStack {
+                List {
+                    ForEach(addplant.items, id:\.self) { item in
+                        Text(item.name ?? "no data")
+                    }
+                    .onDelete(perform: removeItem)
+                }.cornerRadius(8)
+                VStack{
+                    Button("Ajouter une plante") {
+                        isPresentedBool.toggle()
+                    }
+                    .padding()
+                    .background(
+                        LinearGradient(gradient: Gradient(colors: [Color(Constant.Color.magentaUIColor), Color(Constant.Color.blueUIColor)]), startPoint: .top, endPoint: .bottom)
+                    )
+                    .cornerRadius(8)
+                    .foregroundColor(.white)
+                }
+                .navigationBarItems(leading:
+                                        HStack {
+                    Image("logo")
+                        .resizable()
+                        .frame(width: 60, height: 60)
+                        .font(.largeTitle)
+                        .navigationBarTitleDisplayMode(.automatic)
+                   
+                    Text("   Plants Paradise")
+                        .font(Font.custom("Didot", size: 30))
+                })
+            }.padding()
+        }
+        .sheet(isPresented: $isPresentedBool) {
+            AddPlants()
+        }
+    }
+    
+    func removeItem(at offsets: IndexSet) {
+        for index in offsets {
+            let item = addplant.items[index]
+            PersistenceController.shared.delete(item)
+        }
     }
 }
 
+
 struct WelcomeView_Previews: PreviewProvider {
     static var previews: some View {
-        WelcomeView()
+        HeaderView()
     }
 }
+
+
+
