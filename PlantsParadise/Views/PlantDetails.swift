d43ef7e7fcdc7307b7e970748066e87964380be9
Giovanni Gaffe <gaffejonathan@ymail.com>
2021-09-21 18:28:59 +0800

    new file for finalize plant

---
+++ PlantsParadise/Views/PlantDetails.swift
@@ -0,0 +1,20 @@
+//
+//  PlantDetails.swift
+//  PlantsParadise
+//
+//  Created by Giovanni Gaffé on 2021/9/21.
+//
+
+import SwiftUI
+
+struct PlantDetails: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PlantDetails_Previews: PreviewProvider {
+    static var previews: some View {
+        PlantDetails()
+    }
+}
