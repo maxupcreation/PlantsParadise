594749164c17c4d706338fc310e6abf7f27f6697
Giovanni Gaffe <gaffejonathan@ymail.com>
2021-09-24 20:06:47 +0800

    add textfield for tag

--- PlantsParadise/Views/PlantDetails.swift
+++ PlantsParadise/Views/PlantDetails.swift
@@ -15,9 +15,11 @@ struct PlantDetails: View {
     let reminderDays: String
     let plantImage: UIImage
     let gradientColor = GradientColor()
-
+    @State var tagText: String
+    
     var body: some View {
-            HStack(spacing: 30) {
+        VStack {
+            HStack(spacing: 40) {
                 Image(uiImage: plantImage)
                     .resizable()
                     .frame(width: 100, height: 100)
@@ -29,22 +31,29 @@ struct PlantDetails: View {
                         .font(Font.custom("Didot", size: 22))
                         .foregroundColor(.black)
                 } else {
-                Text(flowerName)
-                    .font(Font.custom("Didot", size: 22))
-                    .foregroundColor(.black)
+                    Text(flowerName)
+                        .font(Font.custom("Didot", size: 22))
+                        .foregroundColor(.black)
                 }
                 Text(reminderDays)
                     .frame(width: 50, height: 50)
                     .background(gradientColor.linearGradientBlue())
                     .foregroundColor(.white)
                     .clipShape(Capsule(style: .circular))
+                
             }
+            TextField("Enter your tags", text: $tagText)
+                .padding(.leading, 20)
+                .font(.title3)
+                .textFieldStyle(.roundedBorder)
+                .foregroundColor(.gray)
+        }
         
     }
 }
 
 struct PlantDetails_Previews: PreviewProvider {
     static var previews: some View {
-        PlantDetails(flowerName: "Nom de la plante", reminderDays: "2", plantImage: UIImage(named: "icons8-no_image") ?? UIImage())
+        PlantDetails(flowerName: "Nom de la plante", reminderDays: "2", plantImage: UIImage(named: "icons8-no_image") ?? UIImage(), tagText: "Tags")
     }
 }
